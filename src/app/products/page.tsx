05136005ed03a37fde07ec80602e406d47a4e5c4
Juliherms Vasconcelos <j.a.vasconcelos321@gmail.com>
2025-02-23 09:51:06 -0300

    chore: update app font

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 const ProductPage = () => {
     return (
       <div className="p-5 border border-red-500 roundex-xl">
-        <h1 className="text-red-500">Product Page</h1>;  
+        <h1 className="text-red-500">Product Page</h1>  
         <Button>FSW7.0</Button>
       </div>
     );
